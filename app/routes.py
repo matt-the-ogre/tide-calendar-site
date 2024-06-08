352077475c15a3a738650bbf569899d834a354ed
Matt Manuel <matt-the-ogre@users.noreply.github.com>
2024-06-08 10:00:05 -0700

    auto-save the input station ID in a cookie and re-populate it

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 import subprocess
-from flask import render_template, request, send_file
+from flask import render_template, request, send_file, make_response
 import os
 
 from app import app
@@ -15,9 +15,15 @@ def index():
         subprocess.run(['python', 'get_tides.py', '--station', station_id, '--year', year, '--month', month])
 
         # PDF is saved as "tide_calendar_{station_id}_{year}_{month}.pdf"
-        pdf_filename = f"tide_calendar_{station_id}_{year}_{month}.pdf" # e.g., tide_calendar_9449639_2024_06.pdf
+        pdf_filename = f"tide_calendar_{station_id}_{year}_{month}.pdf"
 
-        # Assuming the PDF is saved as tide_calendar.pdf
-        return send_file(pdf_filename, as_attachment=True)
+        # Create a response object to set the cookie
+        response = make_response(send_file(pdf_filename, as_attachment=True))
+        response.set_cookie('station_id', station_id)
 
-    return render_template('index.html')
+        return response
+
+    # If GET request, read the station_id from the cookie, if available
+    station_id = request.cookies.get('station_id', '9449639')  # Default to '9449639' if no cookie is found
+
+    return render_template('index.html', station_id=station_id)
